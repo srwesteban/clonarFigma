96741eaed724144cf1b2b6650bd6f7cd66a37232
Jhonatan Mideros <jmideros@robinfood.com>
2023-04-19 18:28:06 -0500

    [fix] load components

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div>
+      <h1>Hi! Jonmid - 2013</h1>
+    </div>
+  );
+}
+
+export default App;
