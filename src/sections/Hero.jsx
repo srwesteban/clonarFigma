87c2603cdb14cf6e7a46d7ac961531471bfd8dc1
Jhonatan Mideros <jmideros@robinfood.com>
2023-05-10 18:22:52 -0500

    [update] change responsive in hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HeroData } from '../data/data.js';
 import { LayoutHero } from '../components/Layout';
+import { CloudArrowDownIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
 
 const Hero = () => {
   return (
@@ -16,19 +17,21 @@ const Hero = () => {
                 {HeroData.subTitle}
               </p>
             </section>
-            <section className='flex justify-center gap-x-4 mt-12 lg:justify-start'>
-              <button className='px-5 py-[10px] text-app-white bg-app-accent-500 rounded-[32px]'>
-                {HeroData.btnTextDownloadCv}
+            <section className='flex flex-col gap-y-4 justify-center mt-12 px-6 sm:px-0 sm:gap-y-0 sm:gap-x-4 sm:flex-row lg:justify-start'>
+              <button className='flex flex-row items-center justify-center gap-x-2 px-5 py-[10px] text-app-white bg-app-accent-500 rounded-[32px]'>
+                <span>{HeroData.btnTextDownloadCv}</span>
+                <CloudArrowDownIcon className='w-5 h-5' />
               </button>
-              <button className='px-5 py-[10px] text-app-white bg-app-accent-500 rounded-[32px]'>
-                {HeroData.btnTextContact}
+              <button className='flex flex-row items-center justify-center gap-x-2 px-5 py-[10px] text-app-white bg-app-accent-500 rounded-[32px]'>
+                <span>{HeroData.btnTextContact}</span>
+                <EnvelopeIcon className='w-5 h-5' />
               </button>
             </section>
           </aside>
           <img
             src={HeroData.iconHero}
             alt='ui'
-            className='mt-20 max-w-[425px] shadow-2xl rotate-3'
+            className='mt-20 px-4 max-w-[425px] shadow-2xl sm:px-0 lg:mt-0 lg:rotate-3'
           />
         </section>
       </LayoutHero>
