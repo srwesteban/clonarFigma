704db6aa629c1ef3bab36c6b53a222aa87f32f43
Jhonatan Mideros <jmideros@robinfood.com>
2023-05-10 19:36:56 -0500

    [update] change responsive item service

--- src/components/ItemService.jsx
+++ src/components/ItemService.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const ItemService = ({ data }) => {
+  const orientationLeft = data.orientation === 'left';
+  const reverse = orientationLeft ? 'flex-row' : 'flex-row-reverse';
+  const orientationText = orientationLeft ? 'text-left' : 'text-right';
+
   return (
-    <aside className='flex flex-row items-center justify-between w-[700px]'>
+    <aside
+      className={`flex flex-col items-center justify-between gap-x-8 lg:w-[700px] lg:${reverse}`}
+    >
       <img src={data.icon} alt={data.title} />
-      <section>
+      <section className={`text-center lg:${orientationText}`}>
         <h2 className='text-app-white font-extrabold text-lg'>{data.title}</h2>
-        <p className='mt-3 text-app-white font-light text-sm'>
+        <p className={`mt-3 text-app-white font-light text-sm`}>
           {data.subTitle}
         </p>
       </section>
